5370681e9bcf8309ae7b94dece518be8856ac062
pwinter303 <pwinter303@yahoo.com>
2014-08-13 10:03:42 -0400

    Remove console.log

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -25,13 +25,13 @@ angular.module('todoApp', [
     return {
       response: function(response){
         if (response.status === 401) {
-          console.log('Response 401');
+          //console.log('Response 401');
         }
         return response || $q.when(response);
       },
       responseError: function(rejection) {
         if (rejection.status === 401) {
-          console.log('Response Error 401',rejection);
+          //console.log('Response Error 401',rejection);
           //$location.path('/').search('returnTo', $location.path());
           $location.path('/');
           $rootScope.loggedIn = 0;
