6d81c0f508a2db6f53882218b382e935c6d8d839
pwinter303@yahoo.com <suzy1969>
2014-09-28 20:25:22 -0400

    Major Overhaul Of Main Page... In Progress

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,10 +2,16 @@
 
 // kept this in case code is needed in the main page...
 angular.module('todoApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $location, $anchorScroll) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.scrollTo = function(id) {
+      $location.hash(id);
+      $anchorScroll();
+    }
+
   });
