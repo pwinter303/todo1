92274352b36ae457329eb0570164e1d1ba2e0f37
pwinter303@yahoo.com <suzy1969>
2014-11-01 18:04:58 -0400

    Selenium Script. Deployment Issues

--- app/DB.20141007.patch.sql
+++ app/DB.20141007.patch.sql
@@ -2,17 +2,17 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-ALTER TABLE `db508430361`.`todo`
+ALTER TABLE `todo`
 DROP FOREIGN KEY `fk_todo_todoGroup1`;
 
-ALTER TABLE `db508430361`.`todo`
+ALTER TABLE `todo`
 ADD CONSTRAINT `fk_todo_todoGroup1`
   FOREIGN KEY (`group_id`)
-  REFERENCES `db508430361`.`todo_group` (`group_id`)
+  REFERENCES `todo_group` (`group_id`)
   ON DELETE CASCADE
   ON UPDATE NO ACTION;
 
-CREATE TABLE IF NOT EXISTS `db508430361`.`demo_customer` (
+CREATE TABLE IF NOT EXISTS `demo_customer` (
   `customer_id` INT(11) NOT NULL,
   `last_used_ts` TIMESTAMP NULL,
   `last_reset_ts` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS `db508430361`.`demo_customer` (
   PRIMARY KEY (`customer_id`),
   CONSTRAINT `fk_demo_customer_customer1`
     FOREIGN KEY (`customer_id`)
-    REFERENCES `db508430361`.`customer` (`customer_id`)
+    REFERENCES `customer` (`customer_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -29,8 +29,7 @@ COLLATE = latin1_general_ci;
 CREATE TRIGGER demo_customer_inserts BEFORE INSERT ON demo_customer FOR EACH ROW SET NEW.last_used_ts = CURRENT_TIMESTAMP;
 
 
-ALTER TABLE `db508430361`.`customer`
-ADD COLUMN `referral_email` VARCHAR(145) NULL DEFAULT NULL AFTER `updated_ts`;
+ALTER TABLE `customer` ADD COLUMN `referral_email` VARCHAR(145) NULL DEFAULT NULL AFTER `updated_ts`;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
