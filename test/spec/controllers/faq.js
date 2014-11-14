25c9758fcec32abf2ea1af59325aed9e439067c0
pwinter303 <pwinter303@yahoo.com>
2014-11-14 16:33:11 -0500

    Deployment

--- test/spec/controllers/faq.js
+++ test/spec/controllers/faq.js
@@ -2,13 +2,13 @@
 
 describe('Controller: FaqCtrl', function () {
 
-  // load the controller's module 
+  // load the controller's module
   beforeEach(module('todoApp'));
 
   var FaqCtrl,
     scope;
 
-  // Initialize the controller and a mock scope
+  // Initialize the controller and a mock  scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
     FaqCtrl = $controller('FaqCtrl', {
